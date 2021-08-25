d991cbc9810b3d0ac5492a4cef4f4430cd9c4507
Horacio Leon <horaleon1@gmail.com>
2021-08-25 11:10:31 -0500

    setup redux store

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import HeaderContentPage from "../Ui/HeaderContentPage/HeaderContentPage";
 import LayoutBasePage from "../Layouts/LayoutBasePage/LayoutBasePage";
 import BusinessListItem from "../Ui/BusinessListItem/BusinessListItem";
+import { getUsers } from "../../reducers/businesses";
 
 const Home = () => {
+  const count = useSelector((state) => state).businesses.value;
+  const users = useSelector((state) => state).businesses.businesses;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUsers());
+  }, []);
+
   return (
     <LayoutBasePage>
       <HeaderContentPage title="Business" buttonName="Create business" />
       <BusinessListItem firstColumn="Louis Vuitton" />
-      <BusinessListItem firstColumn="Adidas" secondColumn="Web Designer" />
+      {/* {
+        users.map(user => (
+          <BusinessListItem key={user.id} firstColumn={user.name} secondColumn={user.username} />
+
+        ))
+      } */}
+      {/* <button onClick={() => dispatch(increment())}>Mas</button> */}
+      Count:{count}
     </LayoutBasePage>
   );
 };
